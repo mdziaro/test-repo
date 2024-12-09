fcbb5cd2daaa29bb24d2be215b241bc0a19c2fe6
Mikosztyla <mikosztyla@gmail.com>
2024-12-09 19:31:18 +0100

    nextra version update

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,17 @@
 import nextra from 'nextra';
 
-const nextConfig = {
-    output: 'export',
-    reactStrictMode: true,
-    images: {
-        unoptimized: true
-    },
-    trailingSlash: true
-}
-
 const withNextra = nextra({
     theme: 'nextra-theme-docs',
     themeConfig: './theme.config.tsx',
     latex: true,
     defaultShowCopyCode: true,
-})
+});
 
-export default withNextra(nextConfig);
+export default withNextra({
+    output: 'export',
+    reactStrictMode: true,
+    images: {
+        unoptimized: true,
+    },
+    trailingSlash: true,
+});
